e7719c1f71c49c65dc0f510ce6997f4ca343cfee
IreneSanx <61683259+IreneSanx@users.noreply.github.com>
2020-04-22 16:43:47 +0200

    Update GsonDatabaseClient.java

--- GsonDatabaseClient.java
+++ GsonDatabaseClient.java
@@ -7,7 +7,15 @@ public class GsonDatabaseClient {
 
 	public static void main(String[] args) {
 		try{
-			DatabaseJSonReader dbjp = new DatabaseJSonReader();
+			PosologiesCategory pc = new PosologiesCategory(null);
+			PhysiotherapiesCategory phc = new PhysiotherapiesCategory(pc);
+			MedicinePresentationsCategory mpc = new MedicinePresentationsCategory(phc);
+			RescueMedicineCategory rmc = new RescueMedicineCategory(mpc);
+			ActiveIngredientsCategory aic = new ActiveIngredientsCategory(rmc);
+			MedicinesCategory mc = new MedicinesCategory(aic);
+			
+			DatabaseJSonReader dr = new DatabaseJSonReader(mc);
+			//DatabaseJSonReader dbjp = new DatabaseJSonReader();
 
 			try {
 				System.out.println(dbjp.parse("./datos.json"));

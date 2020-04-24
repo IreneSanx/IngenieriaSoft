e9be318bf0510b65b27478d79ff9b3febc4dcc8d
IreneSanx <61683259+IreneSanx@users.noreply.github.com>
2020-04-24 15:27:20 +0200

    Update GsonDatabaseClient.java

--- GsonDatabaseClient.java
+++ GsonDatabaseClient.java
@@ -6,19 +6,23 @@ import java.io.IOException;
 public class GsonDatabaseClient {
 
 	public static void main(String[] args) {
+		
 		try{
 			PosologiesCategory pc = new PosologiesCategory(null);
 			PhysiotherapiesCategory phc = new PhysiotherapiesCategory(pc);
 			MedicinePresentationsCategory mpc = new MedicinePresentationsCategory(phc);
 			RescueMedicineCategory rmc = new RescueMedicineCategory(mpc);
 			ActiveIngredientsCategory aic = new ActiveIngredientsCategory(rmc);
-			MedicinesCategory mc = new MedicinesCategory(aic);
+			InhalersCategory ic = new InhalersCategory(aic);
+			UserManualPhysioStepsCategory umpsc = new UserManualPhysioStepsCategory(ic);
+			UserManualStepsCategory umsc = new UserManualStepsCategory(umpsc);
+			MedicinesCategory mc = new MedicinesCategory(umsc);
 			
 			DatabaseJSonReader dr = new DatabaseJSonReader(mc);
 			//DatabaseJSonReader dbjp = new DatabaseJSonReader();
 
 			try {
-				System.out.println(dbjp.parse("./datos.json"));
+				System.out.println(dr.parse("./datos.json"));
 			} finally {
 			}
 		} catch (FileNotFoundException e){

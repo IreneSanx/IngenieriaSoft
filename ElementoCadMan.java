1f724d2c4edf4f66f5ec86cd984c11c10f9b37c6
IreneSanx <61683259+IreneSanx@users.noreply.github.com>
2020-04-26 19:49:58 +0200

    Update ElementoCadMan.java

--- ElementoCadMan.java
+++ ElementoCadMan.java
@@ -1,16 +1,34 @@
 package salud.isa.gsonMedBD;
 
+import java.io.IOException;
+
 import com.google.gson.stream.JsonReader;
 
-public class ElementoCadMan {
+public abstract class ElementoCadMan {
 	protected ElementoCadMan siguiente;
 	
 	public ElementoCadMan(ElementoCadMan sig) {
 		siguiente = sig;
 	}
 	
-	public StringBuffer leerCategoria(JsonReader reader, String name) {
+	public StringBuffer leerCategoria(JsonReader reader, String name) throws IOException {
 		return siguiente.leerCategoria(reader,name);
 	}
+	
+	public StringBuffer leerComun(JsonReader reader, String name) throws IOException {
+		StringBuffer data = new StringBuffer();
+		reader.beginArray();
+		while (reader.hasNext()) {
+			reader.beginObject();
+			data.append(readEntry(reader)).append("\n");
+			reader.endObject();
+		}
+		data.append("\n");
+		reader.endArray();
+		return data;
+	}
+
+	public abstract String readEntry(JsonReader reader) throws IOException;
 
 }
+
